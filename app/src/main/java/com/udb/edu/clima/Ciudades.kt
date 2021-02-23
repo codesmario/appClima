6a28001029131ed3ef410fa2728a3a7fc966091f
ogo <mario.sanchez@udb.edu.sv>
2021-02-23 15:55:41 -0600

    funcionamiento de librerio Volley

--- app/src/main/java/com/udb/edu/clima/Ciudades.kt
+++ app/src/main/java/com/udb/edu/clima/Ciudades.kt
@@ -1,12 +1,28 @@
 package com.udb.edu.clima
 
 import android.content.Intent
+import android.net.http.HttpResponseCache
+import android.net.sip.SipSession
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 import android.view.View
+import android.view.textclassifier.TextLinks
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_ciudades.*
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class Ciudades : AppCompatActivity() {
 
@@ -19,6 +35,9 @@ class Ciudades : AppCompatActivity() {
         val bSoya = findViewById<Button>(R.id.bSoyapango)
         var validar = findViewById<Button>(R.id.btnValidarCon)
         var tvWelcome = findViewById<TextView>(R.id.tvWelcome)
+        var tbVolley = findViewById<Button>(R.id.bVolley)
+
+        var btnRequest =  findViewById<Button>(R.id.btnRequest)
 
         validar.setOnClickListener {
             Toast.makeText(this,"En listener",Toast.LENGTH_SHORT)
@@ -40,7 +59,60 @@ class Ciudades : AppCompatActivity() {
 
         })
 
+        btnRequest.setOnClickListener {
+            if (Network.hayRed(this)){
+                Log.d("bSolicitudOnClic", getDownloadData("www.facebook.com"))
+            }
+        }
+
+        //
+        tbVolley.setOnClickListener {
+            if (Network.hayRed(this)) {
+                solicitudHTTPVolley("http://www.udb.edu.sv/udb/noticias/api/last")
+            } else {
+                tvWelcome.text = "No hay una conexion a internet"
+                Toast.makeText(this, "No hay una conexion a internet", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+    //  ./ onCreate
+    @Throws(IOException::class)
+    private fun getDownloadData(url:String):String     {
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        var inputStream:InputStream? = null
 
 
+        try {
+            var url = URL(url)
+            val conection = url.openConnection() as HttpURLConnection
+            conection.requestMethod = "GET"
+            conection.connect()
+
+            inputStream = conection.inputStream
+            return  inputStream.bufferedReader().use {
+                it.readText()
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close()
+            }
+        }
+    }
+    // ./ getDownloadData
+
+    private fun solicitudHTTPVolley(url:String) {
+        val queue = Volley.newRequestQueue(this)
+        val solicitud = StringRequest(Request.Method.GET , url, {
+            response ->
+            try {
+                Log.d("solicitudHTTPVolley", response)
+            } catch ( e: Exception) {
+
+            }
+        }, {  })
+        queue.add(solicitud)
     }
 }
---
+++ app/src/main/java/com/udb/edu/clima/Descarga.kt
@@ -0,0 +1,4 @@
+package com.udb.edu.clima
+
+class Descarga {
+}
